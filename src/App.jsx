7ad6af69a8a46eddda574da33c5a8ddc032b9219
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-05-29 02:00:19 -0400

    added code for card containers and pages for about portfolio and contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Spinner from './components/spinner'
 import NavBar from './components/navbar' 
-import './assets/scss/bootstrap.scss'
-
+// import './assets/scss/bootstrap.scss'
+import Home from './pages/home'
+import Portfolio from './pages/portfolio'
+import About from './pages/about'
+import Contact from './pages/contact'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <>
-      <Spinner/>
-      <NavBar/>
+      
+      <NavBar currentPage={currentPage} handlePageChange={handlePageChange}/>
+      {renderPage()}
     </>
   )
 }
---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,15 @@
+import CardContainer from "../components/cardContainer"
+import Card from '../components/card'
+import projects from '../assets/data/projects.json'
+
+function Portfolio(){
+    return <>
+        <CardContainer>
+            {projects.map(project=>{
+                return <Card project={project}/>
+            })}
+           
+        </CardContainer>
+    </>
+}
+export default Portfolio 
